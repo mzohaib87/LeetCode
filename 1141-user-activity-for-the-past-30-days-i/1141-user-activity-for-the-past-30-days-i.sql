30a8a1ac8a4348a56d3a99417ea7693e50b1ca4b
Zohaib Nasir <mzohaib94@gmail.com>
2023-01-16 07:32:26 +0500

    Time: 1708 ms (78.38%), Space: 0B (100.00%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+
+select SQ1.day, SQ1.active_users from
+(
+Select activity_date as day,count(distinct user_id) as active_users
+from Activity
+where activity_date > '2019-06-27' and activity_date <= '2019-07-27'
+group by activity_date
+) SQ1
+
