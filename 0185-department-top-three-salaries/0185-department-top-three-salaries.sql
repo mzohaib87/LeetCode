d73995d33169d502d09f5e328fc214c4f101ab38
Zohaib Nasir <mzohaib94@gmail.com>
2023-01-16 07:14:46 +0500

    Time: 1014 ms (31.93%), Space: 0B (100.00%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+
+
+Select d.name as Department ,SQ1.name as Employee ,salary as Salary  from
+(Select name,salary,departmentId,Dense_Rank() over(partition by departmentId order by salary desc) as RankNo
+from Employee) SQ1 join Department d
+on SQ1.departmentId=d.id
+where RankNo<=3;
+
