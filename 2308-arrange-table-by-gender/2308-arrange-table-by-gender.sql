bb49bba0f7fb4add5c8a959bf31e9d0862bc8e8a
Zohaib Nasir <mzohaib94@gmail.com>
2023-01-16 08:04:38 +0500

    Time: 1196 ms (52.94%), Space: 0B (100.00%) - LeetHub

---
+++ 2308-arrange-table-by-gender/2308-arrange-table-by-gender.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+
+SELECT user_id, gender
+FROM Genders
+ORDER BY ROW_NUMBER() OVER(PARTITION BY gender ORDER BY user_id), 
+		CASE WHEN gender='female' THEN 1 WHEN gender='other' THEN 2 WHEN gender='male' THEN 3 END;
+
